f9579e20d31b7d42df821ffaf80167ce4bcec124
Justin Shayesteh <justinshayesteh@yahoo.com>
2016-05-25 21:17:16 -0400

    soclose

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ document.getElementById("food").innerHTML = myFavoriteFood;
 var list = document.querySelectorAll('li');
 
 for (var a = 0; a<= list.length; a++){
+  list[a].setAttribute( "class", "listitem");
   list[a].style.color = "red";
-  list[a].className = "listItem";
 }
 
-// var img = document.createElement("img");
-// img.setAttribute('src', ‘https://avatars0.githubusercontent.com/u/18503825?v=3&s=460');
-// img.setAttribute('alt', 'about me');
-// img.setAttribute('height', '50%');
-// img.setAttribute('width', '50%');
-// document.body.appendChild(img);
+var img = document.createElement("img");
+img.setAttribute("src", "http://www.signature-reads.com/wp-content/uploads/2015/03/Belushi.jpg");
+img.setAttribute("alt", "about me");
+img.setAttribute("height", "50%");
+img.setAttribute("width", "50%");
+document.body.appendChild(img);
